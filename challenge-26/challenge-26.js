10c8cddcdf5759ab0d76124de11ee227211eee2e
fernando-1996 <fernandoargentino@outlook.com>
2021-05-05 14:00:48 -0300

    Update challenge-26.js

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,31 +1,34 @@
-/*
-O desafio dessa semana é criar uma mini library (biblioteca) para
-reutilizarmos nossos códigos quando fizermos manipulação de DOM!
+function on(event, callback) {
+		Array.prototype.forEach.call(this.element, function(item, index) {
+			item.addEventListener('click', callback, false);
+		});
+	}
+	
+	function off(event, callback) {
+		Array.prototype.forEach.call(this.element, function(item, index) {
+			item.removeEventListener('click', callback, false);
+		});
+	}
+	
+	function get() {
+		return this.element;
+	}
+	
+	function DOM(string) {
+		this.element = document.querySelectorAll(string);
+	}
+	DOM.prototype.on = on;
+	DOM.prototype.off = off;
+	DOM.prototype.get = get;
+	
+	
+	
+	var $a = new DOM('[data-js="link"]');
+	
+	$a.on('click', function(e) {
+	  e.preventDefault();
+	  console.log('clicou');
+	});
 
-Requisitos:
-- O nome da lib deve ser "DOM".
-- Ela deve ser uma função construtora, que receberá uma string por parâmetro.
-Essa string será o nó do DOM a ser selecionado;
-- No construtor, você deve atribuir à `this.element` todos os elementos
-do DOM selecionados;
-- Extenda a lib para ter os métodos `on`, `off` e `get`.
-- O método `on` irá adicionar um listener de evento a todos os elementos
-selecionados.
-- O método `off` irá remover o listener de evento de todos os elementos
-selecionados.
-- O método `get` deve retornar os elementos.
-- O código abaixo deve funcionar corretamente após a lib criada.
-
-Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
-Só passe para o próximo problema quando tiver resolvido o anterior :)
-*/
-// ?
-
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
-
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+	console.log('Elementos selecionados:', $a.get());
+	console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
